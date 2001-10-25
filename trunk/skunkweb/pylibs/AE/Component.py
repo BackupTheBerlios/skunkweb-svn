b39cec178b9f17f1a331a4683718cfa9b70c7773
drew_csillag <drew_csillag@b0d435bb-e9e7-0310-90fb-840138c28313>
2001-10-25 01:27:53 +0000

    fixed so expiration actually works properly

--- trunk/skunkweb/pylibs/AE/Component.py
+++ trunk/skunkweb/pylibs/AE/Component.py
@@ -15,7 +15,7 @@
 #      along with this program; if not, write to the Free Software
 #      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
 #   
-# $Id: Component.py,v 1.5 2001/08/27 19:52:51 drew_csillag Exp $
+# $Id: Component.py,v 1.6 2001/10/25 01:27:53 drew_csillag Exp $
 # Time-stamp: <2001-07-10 12:20:38 drew>
 ########################################################################
 
@@ -352,8 +352,9 @@ def _realRenderComponent( name, argDict, auxArgs, compType, srcModTime ):
 
     # normally this is equivalent to a pop, but if a component exception
     # was handled somewhere, we need to clean up the residual shit
+    ns_copy = namespace.copy( )
     del componentStack[ topOfComponentStack+1 : ]
-
+    namespace = ns_copy
     expiration = namespace.get('__expiration',
                                time.time()
                                + cfg.Configuration.defaultExpiryDuration )
@@ -384,6 +385,9 @@ def _getAuxArgs( argDict ):
 
 ########################################################################
 # $Log: Component.py,v $
+# Revision 1.6  2001/10/25 01:27:53  drew_csillag
+# fixed so expiration actually works properly
+#
 # Revision 1.5  2001/08/27 19:52:51  drew_csillag
 # commented out more DEBUG statements
 #
