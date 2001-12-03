7c35c561c68032921950f972c8c00b2f9c640d02
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2001-12-03 02:45:05 +0000

    modified mysql service to use MySQLdb, and merged in connection caching code from the MySQL pylib.  Adjusted sw.conf.in accordingly.

--- trunk/skunkweb/SkunkWeb/Services/mysql/__init__.py
+++ trunk/skunkweb/SkunkWeb/Services/mysql/__init__.py
@@ -1,4 +1,7 @@
-#  
+# Time-stamp: <01/12/02 21:04:49 smulloni>
+# $Id: __init__.py,v 1.2 2001/12/03 02:45:05 smulloni Exp $
+
+########################################################################  
 #  Copyright (C) 2001 Andrew T. Csillag <drew_csillag@geocities.com>
 #  
 #      This program is free software; you can redistribute it and/or modify
@@ -14,14 +17,72 @@
 #      You should have received a copy of the GNU General Public License
 #      along with this program; if not, write to the Free Software
 #      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
-#   
+#
+########################################################################
+
 from SkunkWeb import Configuration
-import MySQL
+import MySQLdb
+from SkunkExcept import SkunkStandardError
+
+_users={}
+_connections = {}
+
+def initUser (connUser, **connParams):
+    """
+    Prior to using the module, you must call this function 
+    to add a user and associate it with connect parameters. 
+    The function can be called more than once.
+    """
+    if _users.has_key (connUser):
+        raise SkunkStandardError, 'user %s already initialized!' % (connUser) 
+    _users[connUser] = connParams
+
+def cleanUser (connUser):
+    """
+    Destroys the database connection defined by connUser.
+    If there is no database connection open for connUser,
+    a SkunkStandardError is thrown.
+    """
+    if not _users.has_key (connUser):
+        raise SkunkStandardError, 'user %s is not initialized' % (connUser)
+    del _users[connUser]
+
+def getConnection(connUser):
+    """
+    Returns a database connection as defined by 
+    connUser. If this module already has an open
+    connection for connUser, it returns it; otherwise,
+    it creates a new connection, stores it, and returns it.
+    """
+
+    if not _users.has_key (connUser):
+        raise SkunkStandardError, 'user %s is not initialized' % (connUser)
+
+    if not _connections.has_key(connuser):
+        connectParams = _users[connUser]
+        try:
+            _connections[connuser] = MySQLdb.connect(**connectParams)
+        except MySQLdb.MySQLError:
+            # XXX Do not raise the connect string! The trace may be seen
+            # by users!!!
+            raise SkunkStandardError, ('cannot connect to MySQL: %s' % 
+                  (sys.exc_info()[1],))
+    return _connections[connuser]
+
+########################################################################
 
 Configuration.mergeDefaults(
     MySQLConnectParams = {},
     )
 
 for u, p in Configuration.MySQLConnectParams.items():
-    MySQL.initUser ( u, p )
+    initUser(u, **p)
 
+########################################################################
+# $Log: __init__.py,v $
+# Revision 1.2  2001/12/03 02:45:05  smulloni
+# modified mysql service to use MySQLdb, and merged in
+# connection caching code from the MySQL pylib.  Adjusted
+# sw.conf.in accordingly.
+#
+########################################################################
