b4f0235df342205eba8bb5460a0827f702381f82
drew_csillag <drew_csillag@b0d435bb-e9e7-0310-90fb-840138c28313>
2002-06-24 21:49:46 +0000

    forgot to trim the component stack when rendering the tag traceback

--- trunk/skunkweb/pylibs/AE/Error.py
+++ trunk/skunkweb/pylibs/AE/Error.py
@@ -15,7 +15,7 @@
 #      along with this program; if not, write to the Free Software
 #      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
 #   
-#$Id: Error.py,v 1.1 2001/08/05 15:00:41 drew_csillag Exp $
+#$Id: Error.py,v 1.2 2002/06/24 21:49:46 drew_csillag Exp $
 import cStringIO
 
 import ErrorHandler
@@ -29,7 +29,7 @@ def logException():
     ErrorHandler.logError(out)
     
     errorTags = [frame.executable.dt._error_tag
-                 for frame in Component.componentStack
+                 for frame in Component.componentStack[:Component.topOfComponentStack]
                  if isinstance(frame.executable, Executables.STMLExecutable)
                  if hasattr(frame.executable.dt, '_error_tag')]
     if errorTags:
