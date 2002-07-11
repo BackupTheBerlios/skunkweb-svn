4e470c3ba9803212a081279e912d2621af5d64f1
drew_csillag <drew_csillag@b0d435bb-e9e7-0310-90fb-840138c28313>
2002-07-11 19:21:55 +0000

    added CGI_JOB

--- trunk/skunkweb/SkunkWeb/SkunkWeb/constants.py
+++ trunk/skunkweb/SkunkWeb/SkunkWeb/constants.py
@@ -15,7 +15,7 @@
 #      along with this program; if not, write to the Free Software
 #      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
 #   
-# $Id: constants.py,v 1.2 2001/10/02 00:06:35 smulloni Exp $
+# $Id: constants.py,v 1.3 2002/07/11 19:21:55 drew_csillag Exp $
 # Time-stamp: <01/05/04 13:31:34 smulloni>
 ########################################################################
 
@@ -44,9 +44,12 @@ WEB_JOB="/web/"
 AE_COMPONENT_JOB='/ae_component/'
 REMOTE_JOB=AE_COMPONENT_JOB + "/remote/"
 TEMPLATING_JOB= WEB_JOB + AE_COMPONENT_JOB + '/templating/'
-
+CGI_JOB = WEB_JOB + '/cgi/'
 ########################################################################
 # $Log: constants.py,v $
+# Revision 1.3  2002/07/11 19:21:55  drew_csillag
+# added CGI_JOB
+#
 # Revision 1.2  2001/10/02 00:06:35  smulloni
 # fixes for unix sockets, which were broken due to profound cognitive
 # impairment.
