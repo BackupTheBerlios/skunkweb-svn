ab2e89efdd3ed6314368e149d72ccd38d05948b5
drew_csillag <drew_csillag@b0d435bb-e9e7-0310-90fb-840138c28313>
2002-07-29 15:23:13 +0000

    added

---
+++ trunk/skunkweb/pylibs/psp.py
@@ -0,0 +1,39 @@
+CODE, LITERAL = ('CODE', 'LITERAL')#range(2)
+
+def psp_compile(s, name):
+    """<% and %> are the code delimiters"""
+    l = []
+    while 1:
+        ci = s.find('<%')
+        if ci == -1:
+            l.append((LITERAL, s))
+            break
+        l.append((LITERAL,s[:ci]))
+        code = s[ci+2:]
+        #print 'code is ', code
+        ce = code.find('%>')
+        if ce == -1:
+            raise 'EOFError', 'End of file reached while in code'
+        code = code[:ce]
+        l.append((CODE, code))
+        s = s[ci + 2 + ce+2:]
+
+    codestr = ''
+    for kind, s in l:
+        #print kind, repr(s)
+        if kind == LITERAL:
+            codestr+='print %s' % repr(s)
+        else:
+            codestr+=s
+    #print 'codestr is', codestr
+    return compile(codestr, name, 'exec'), codestr
+
+if __name__=='__main__':
+    t = """Counting to 3<%
+for i in range(3):
+    print i
+%>
+
+done counting"""
+    co = psp_compile(t,'<string>')
+    exec co[0] in {}
