58f9fa4201abc5421b8b4acb7ff34dd5cbaafeac
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2002-09-30 15:44:50 +0000

    added test data

--- trunk/skunkweb/docs/tutorial/src/sitelibs/hoptime.py
+++ trunk/skunkweb/docs/tutorial/src/sitelibs/hoptime.py
@@ -1,19 +1,30 @@
-# Time-stamp: <>
-# $Id: hoptime.py,v 1.1 2002/09/30 01:53:44 smulloni Exp $
+# Time-stamp: <02/09/30 00:44:44 smulloni> 
+# $Id: hoptime.py,v 1.2 2002/09/30 15:44:50 smulloni Exp $
 
 import PyDO
-    
-PyDO.DBIInitAlias('hoptime', 'pydo:postgresql:localhost:hoptime:postgres')
 
-class Players(PyDO.PyDO):
-    connectionAlias = 'hoptime'
+DEFAULT_CONNECTSTRING='pydo:postgresql:localhost:hoptime:postgres'
+
+def initDB(connectstring=DEFAULT_CONNECTSTRING):
+    PyDO.DBIInitAlias('hoptime', connectstring)
+
+class _hoptimebase(PyDO.PyDO):
+    connectionAlias='hoptime'
+
+class GameStateException(Exception): pass
+
+rollback=_hoptimebase.rollback
+commit=_hoptimebase.commit
+getDBI=_hoptimebase.getDBI
+
+class Players(_hoptimebase):
     table = 'players'
     fields = (
         ('player', 'integer'),
         ('game', 'integer'),
         ('joined', 'timestamp'),
-        ('play_number', 'integer')
-    )
+        ('play_number', 'integer'),
+        )
     unique = [('player', 'game')]
 
     def getGame(self):
@@ -31,33 +42,7 @@ class Players(PyDO.PyDO):
                              player = self['player'])
 
 
-class Edits(PyDO.PyDO):
-    connectionAlias = 'hoptime'
-    table = 'edits'
-    fields = (
-        ('player', 'integer'),
-        ('game', 'integer'),
-        ('content_diff', 'text'),
-        ('id', 'integer'),
-        ('entered', 'timestamp'),
-    )
-    unique = [('id',)]
-
-    sequenced = {
-        'id': 'edits_id_seq',
-    }
-    def getPlayer(self):
-        return Players.getUnique(player = self['player'],
-                                 game=self['game'])
-
-    def getUser(self):
-        return Users.getUnique(id=self['player'])
-
-    def getGame(self):
-        return Games.getUnique(id=self['game'])
-
-class Moves(PyDO.PyDO):
-    connectionAlias = 'hoptime'
+class Moves(_hoptimebase):
     table = 'moves'
     fields = (
         ('content_append', 'text'),
@@ -65,15 +50,14 @@ class Moves(PyDO.PyDO):
         ('game', 'integer'),
         ('entered', 'timestamp'),
         ('id', 'integer'),
-    )
+        )
     unique = [('id',)]
 
     sequenced = {
         'id': 'moves_id_seq',
-    }
+        }
 
-class Games(PyDO.PyDO):
-    connectionAlias = 'hoptime'
+class Games(_hoptimebase):
     table = 'games'
     fields = (
         ('owner', 'integer'),
@@ -84,26 +68,70 @@ class Games(PyDO.PyDO):
         ('status', 'varchar'),
         ('id', 'integer'),
         ('created', 'timestamp'),
-    )
-    unique = [('id',)]
+        )
+    unique = [('id',), ('title',)]
 
     sequenced = {
         'id': 'games_id_seq',
-    }
+        }
     def getPlayers(self):
         return Players.getSome(game = self['id'])
 
     def addPlayer(self, user):
+        if self['status']!='joining':
+            raise "game closed"
         Players.new(game=self['id'], player=user['id'])
 
     def getOwner(self):
         return Users.getUnique(id = self['owner'])
 
     def getUsers(self):
-        return self.joinTable('id', "players", 'game', 'player', Users, 'id')
+        return self.joinTable('id',
+                              "players",
+                              'game',
+                              'player',
+                              Users,
+                              'id')
 
-class Users(PyDO.PyDO):
-    connectionAlias = 'hoptime'
+    def getText(self, before_edit=0):
+        if not before_edit and self['status'] in ('editing', 'published'):
+            s=Stories.getUnique(game=self['id'])
+            if s:
+                return s['story']
+            else:
+                # shouldn't happen
+                return None
+        else:
+            sql='SELECT get_text(%d)' % self['id']
+            c=self.getDBI().conn.cursor()
+            c.execute(sql)
+            t=c.fetchone()
+            c.close()
+            return t
+
+    def start(self):
+        if self['status']=='joining':
+            self['status']='playing'
+        else:
+            raise GameStateException, "game cannot be started"
+
+    def edit(self):
+        if self['status']=='playing':
+            self['status']='editing'
+        else:
+            raise GameStateException, "game cannot be edited"
+
+    def publish(self):
+        if self['status'] in ('editing', 'playing'):
+            self['status']='published'
+        else:
+            raise GameStateException, "game cannot be published"
+
+    def trash(self):
+        self['status']='trashed'
+        
+
+class Users(_hoptimebase):
     table = 'users'
     fields = (
         ('username', 'text'),
@@ -114,12 +142,12 @@ class Users(PyDO.PyDO):
         ('password', 'text'),
         ('id', 'integer'),
         ('email', 'text'),
-    )
+        )
     unique = [('id',), ('username',)]
 
     sequenced = {
         'id': 'users_id_seq',
-    }
+        }
     def getPlayers(self):
         return Players.getSome(player = self['id'])
 
@@ -129,3 +157,20 @@ class Users(PyDO.PyDO):
     def getPlayedGames(self):
         return self.joinTable('id', "players", 'player', 'game', Games, 'id')
 
+    def move(self, game, text):
+        return Moves.new(refetch=1, player=self['id'], game=game['id'], content_append=text)
+
+class Stories(_hoptimebase):
+    table='stories'
+    fields=(
+        ('id', 'integer'),
+        ('game', 'integer'),
+        ('story', 'text'),
+        )
+    unique=[('id',), ('game',)]
+    sequenced={
+        'id' : 'stories_id_seq',
+        }
+
+    def getGame(self):
+        return Games.getUnique(id=self['game'])
