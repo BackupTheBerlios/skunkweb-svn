0bfe4e63a10099ee061a1f8db244289f65e0cbfa
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2002-11-19 21:39:08 +0000

    in the midst of being modified.

--- trunk/skunkweb/pylibs/formlib/state.py
+++ trunk/skunkweb/pylibs/formlib/state.py
@@ -1,38 +1,96 @@
 import base64
 import cPickle
-import md5
+from md5 import md5
+
+class InvalidStateException(Exception): pass
+
+class ArmorEncoder:
+    def __init__(self, nonce):
+        self.nonce=nonce
+        
+    def encode(self, thing):
+        s = cPickle.dumps(thing, 1)
+        hash = md5(self.nonce + s).digest()
+        outv = base64.encodestring(hash + s)
+        return ''.join(outv.split('\n'))
+
+    def decode(self, thingstr):
+        inv = base64.decodestring(thingstr)
+        hash, pick = inv[:16], inv[16:]
+        nhash = md5.md5(self.nonce + pick).digest()
+        if nhash != hash:
+            raise InvalidStateException, 'state has been tampered with'
+        return cPickle.loads(pick)
+    
+try:
+    import Crypto.Cipher.AES as AES
+except ImportError:
+    AES=None
+
+if AES:
+    class AESEncoder:
+        def __init__(self, key, IV=None):
+            self.key=key
+            self.__key=md5(key).digest()
+            if IV=None:
+                IV=self.key[:16].ljust(16)
+            self.aes=AES.new(self.__key, AES.MODE_CBC, IV)
+
+        def _pad(self, thing):
+            s='%dX%s' % (len(thing), thing)
+            ls=len(s)
+            m=ls % AES.block_size
+            return s.ljust(ls-m + AES.block_size)
+
+        def _unpad(self, padded):
+            xind=s.find('X')
+            num=int(s[:xind])
+            return s[xind+1:xind+1+num]
+         
+
+        def encode(self, thing):
+            thing=self._pad(thing)
+            return self.aes.encrypt(thing)
+
+        def decode(self, encoded):
+            thing=self.aes.decrypt(encoded)
+            return self._unpad(thing)
+            
+
 
 class InPageStateManager:
-    def __init__(self, nonce):
+    def __init__(self, nonce, stateVariable='_state'):
         self.state = {'stack' : []}
 
         #these are non-persisted state items
         self.nonce = nonce
-        self.invalidFields = None
-        self.invalidOtherFields = None
+        self.stateVariable=stateVariable
+
         
-    def asHTML(self):
+    def write(self):
         s = cPickle.dumps(self.state, 1)
         hash = md5.md5(self.nonce + s).digest()
         outv = base64.encodestring(hash + s)
-        outv = ''.join(outv.split('\n'))
-        return '<input type=hidden name=_state value="%s">' % outv
+        return ''.join(outv.split('\n'))
 
-    def setstate(self, cgiarguments):
-        try:
-            state = cgiarguments['_state']
-        except:  #no state to get
-            return
-        inv = base64.decodestring(state)
+    def read(self, statestr):
+        inv = base64.decodestring(statestr)
         hash, pick = inv[:16], inv[16:]
         nhash = md5.md5(self.nonce + pick).digest()
         if nhash != hash:
-            return 'state has been tampered with'
+            raise InvalidStateException, 'state has been tampered with'
         self.state = cPickle.loads(pick)
+
+    def setstate(self, cgiarguments):
+        try:
+            statestr = cgiarguments[self.stateVariable]
+        except:  # no state to get
+            return
+        self.read(statestr)
         
-    def push(self, formname):
+    def push(self, formdata):
         stack = self.state['stack']
-        stack.append(formname)
+        stack.append(formdata)
 
     def pop(self):
         stack = self.state['stack']
