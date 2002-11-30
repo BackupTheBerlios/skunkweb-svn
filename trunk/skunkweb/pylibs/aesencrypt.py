5c71502a3b71a8cb374b1a99995b054117c972c7
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2002-11-30 02:11:34 +0000

    added command-line file encryption/decryption to aesencrypt.py.

--- trunk/skunkweb/pylibs/aesencrypt.py
+++ trunk/skunkweb/pylibs/aesencrypt.py
@@ -1,5 +1,6 @@
-# Time-stamp: <02/11/20 12:11:39 smulloni>
-# $Id: aesencrypt.py,v 1.1 2002/11/20 17:34:11 smulloni Exp $
+#! /usr/bin/env python
+# Time-stamp: <02/11/29 20:59:36 smulloni>
+# $Id: aesencrypt.py,v 1.2 2002/11/30 02:11:34 smulloni Exp $
 """
 a convenience class for encrypting strings or
 arbitrary length using AES.  Requires PyCrypto,
@@ -7,7 +8,8 @@ available at from http://www.amk.ca/python/code/crypto.html.
 """
 import Crypto.Cipher.AES as AES
 import md5
-
+import os
+import sys
 
 class AESEncryptor:
     def __init__(self, key, IV=None):
@@ -35,3 +37,85 @@ class AESEncryptor:
     def decrypt(self, encrypted):
         thing=AES.new(self.__key, AES.MODE_CBC, self.IV).decrypt(encrypted)
         return self._unpad(thing)
+
+
+def encrypt_file(infp, outfp, key):
+    if infp=='-':
+        infp=sys.stdin
+    else:
+        infp=open(infp, 'rb')
+        
+    if  outfp=='-':
+        outfp=sys.stdout
+    else:
+        outfp=open(outfp, 'wb')
+    slurp=infp.read()
+    encoder=AESEncryptor(key)
+    inscrut=encoder.encrypt(slurp)
+    outfp.write(inscrut)
+    outfp.flush()
+    outfp.close()
+
+def decrypt_file(infp, outfp, key):
+    if infp=='-':
+        infp=sys.stdin
+    else:
+        infp=open(infp, 'rb')
+        
+    if  outfp=='-':
+        outfp=sys.stdout
+    else:
+        outfp=open(outfp, 'wb')
+    slurp=infp.read()
+    encoder=AESEncryptor(key)
+    inscrut=encoder.decrypt(slurp)
+    outfp.write(inscrut)
+    outfp.flush()
+    outfp.close()
+    
+if __name__=='__main__':
+    import getopt
+    usage=("usage: %s [-h] [-x|-c] <input file> <output file>\n" \
+           "where options are:\n" \
+           "\t-h            print this help message\n" \
+           "\t-x            decrypt\n" \
+           "\t-c            encrypt (default)\n") % sys.argv[0]
+    optlist, args=getopt.getopt(sys.argv[1:], "cxh")
+    opts=dict(optlist)
+    help=opts.has_key('-h')
+    if help:
+        print usage
+        sys.exit(1)
+   
+    decrypt=opts.has_key('-x')
+    encrypt=opts.has_key('-c')
+    if decrypt and encrypt:
+        print "decrypt and encrypt are mutually incompatible options!"
+        print usage
+        sys.exit(1)
+    if not (decrypt or encrypt):
+        encrypt=1
+    # verify files
+    if len(args)==0:
+        infp='-'
+        outfp='-'
+    elif len(args)==1:
+        outfp='-'
+        infp=args[0]
+    else:
+        infp, outfp=args[:2]
+    if infp!='-':
+        if not os.path.exists(infp):
+            print >> sys.stderr, "file %s not found!" % infp
+            print >> sys.stderr, usage
+            sys.exit(1)
+    import getpass
+    key=getpass.getpass('encryption key:')
+    if encrypt:
+        encrypt_file(infp, outfp, key)
+    else:
+        decrypt_file(infp, outfp, key)
+        
+    
+    
+        
