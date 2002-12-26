f1a6f2a043af41af6da3ad41fcad4acc32f116fb
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2002-12-26 01:10:29 +0000

    added Drew to copyright.

--- trunk/skunkweb/pylibs/formlib/dispatcher.py
+++ trunk/skunkweb/pylibs/formlib/dispatcher.py
@@ -1,5 +1,24 @@
-# Time-stamp: <02/12/05 15:39:33 smulloni>
-# $Id: dispatcher.py,v 1.9 2002/12/05 20:40:38 smulloni Exp $
+# Time-stamp: <02/12/13 11:07:00 smulloni>
+# $Id: dispatcher.py,v 1.10 2002/12/26 01:10:29 smulloni Exp $
+
+######################################################################## 
+#  Copyright (C) 2002 Jacob Smullyan <smulloni@smullyan.org>,
+#                     Drew Csillag <drew_csillag@yahoo.com>
+#  
+#      This program is free software; you can redistribute it and/or modify
+#      it under the terms of the GNU General Public License as published by
+#      the Free Software Foundation; either version 2 of the License, or
+#      (at your option) any later version.
+#  
+#      This program is distributed in the hope that it will be useful,
+#      but WITHOUT ANY WARRANTY; without even the implied warranty of
+#      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#      GNU General Public License for more details.
+#  
+#      You should have received a copy of the GNU General Public License
+#      along with this program; if not, write to the Free Software
+#      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
+########################################################################
 
 from containers.fieldcontainer import FieldContainer
 from form import _getname
