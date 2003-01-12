ee2343bb88c8317b6e7c68173c77c6ca86aa74e3
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2003-01-12 18:02:17 +0000

    added acknowledgement of John Derrick for pypgsql driver.

--- trunk/skunkweb/pylibs/PyDO/pypgsqlconn.py
+++ trunk/skunkweb/pylibs/PyDO/pypgsqlconn.py
@@ -16,6 +16,8 @@
 #      Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111, USA.
 ########################################################################
 
+# contributed by John Derrick.
+
 """
 currently unsupported types:
 
