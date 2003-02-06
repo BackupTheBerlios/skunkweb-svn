8d0d5d3ad84689bcea8bc1703a87959dfa0d28d4
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2003-02-06 19:53:56 +0000

    allow verbosity to be specified in a MySQL connectstring

--- trunk/skunkweb/pylibs/PyDO/mysqlconn.py
+++ trunk/skunkweb/pylibs/PyDO/mysqlconn.py
@@ -37,7 +37,7 @@ class PyDOMySQL:
 
     def __unpackConnectArgs(self, connectArgs):
         if type(connectArgs) in (types.StringType, types.UnicodeType):
-            fields='host', 'db', 'user', 'passwd', 'cacheUser'
+            fields='host', 'db', 'user', 'passwd', 'cacheUser', 'verbose'
             d={}
             for k, v in zip(fields, connectArgs.split(':')):
                 d[k]=v
