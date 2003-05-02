eae3674ac2e25499e93d9d2598eb3c02c502122d
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2003-05-02 17:39:37 +0000

    removing cruft

--- trunk/skunkweb/SkunkWeb/SkunkWeb/Hooks.py
+++ trunk/skunkweb/SkunkWeb/SkunkWeb/Hooks.py
@@ -1,5 +1,5 @@
-# Time-stamp: <02/02/10 16:32:19 smulloni>
-# $Id: Hooks.py,v 1.9 2003/05/01 20:45:55 drew_csillag Exp $
+# Time-stamp: <03/05/02 13:39:12 smulloni>
+# $Id: Hooks.py,v 1.10 2003/05/02 17:39:37 smulloni Exp $
 
 ########################################################################
 #  
@@ -15,55 +15,3 @@ from hooks import Hook, KeyedHook
 
 ChildStart = Hook()
 ServerStart = Hook()
-
-
-########################################################################
-# $Log: Hooks.py,v $
-# Revision 1.9  2003/05/01 20:45:55  drew_csillag
-# Changed license text
-#
-# Revision 1.8  2002/02/14 02:58:25  smulloni
-# moved hooks into a pylib; added some logging to templating handler, and minor fix
-# to web service.
-#
-# Revision 1.7  2001/09/19 02:11:52  smulloni
-# fixed typo
-#
-# Revision 1.6  2001/09/09 02:37:41  smulloni
-# performance enhancements, removal of sundry nastinesses and erasure of
-# reeking rot.
-#
-# Revision 1.5  2001/08/27 18:37:05  drew_csillag
-# Only
-# put out DEBUG msg if DEBUGIT.
-#
-# Revision 1.4  2001/08/12 18:11:23  smulloni
-# better fix to DEBUG import problem
-#
-# Revision 1.3  2001/08/12 01:35:31  smulloni
-# backing out previous change, which broke everything
-#
-# Revision 1.2  2001/08/11 23:39:45  smulloni
-# fixed a maladroit import-from inside a frequently called method
-#
-# Revision 1.1.1.1  2001/08/05 14:59:37  drew_csillag
-# take 2 of import
-#
-#
-# Revision 1.8  2001/07/09 20:38:40  drew
-# added licence comments
-#
-# Revision 1.7  2001/04/10 22:48:30  smullyan
-# some reorganization of the installation, affecting various
-# makefiles/configure targets; modifications to debug system.
-# There were numerous changes, and this is still quite unstable!
-#
-# Revision 1.6  2001/04/04 18:11:37  smullyan
-# KeyedHooks now take glob as keys.  They are tested against job names with
-# fnmatch.fnmatchcase.   The use of '?' is permitted, but discouraged (it is
-# also pointless).  '*' is your friend.
-#
-# Revision 1.5  2001/04/04 14:46:31  smullyan
-# moved KeyedHook into SkunkWeb.Hooks, and modified services to use it.
-#
-########################################################################
