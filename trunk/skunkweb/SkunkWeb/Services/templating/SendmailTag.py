2f31edad9aacfb9417f500cb065921d6a2fcd5f2
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2003-05-10 03:05:37 +0000

    now accepts extended arguments, as per MailServices.sendmail

--- trunk/skunkweb/SkunkWeb/Services/templating/SendmailTag.py
+++ trunk/skunkweb/SkunkWeb/Services/templating/SendmailTag.py
@@ -6,9 +6,9 @@
 #      README file.
 #   
 """
-For now, just contain the sendmail tag
+contains the sendmail tag.
 """
-# $Id: SendmailTag.py,v 1.3 2003/05/01 20:45:54 drew_csillag Exp $
+# $Id: SendmailTag.py,v 1.4 2003/05/10 03:05:37 smulloni Exp $
 
 from AE.CommonStuff import *
 import MailServices
@@ -18,23 +18,39 @@ class SendmailTag(DTTag):
     Send mail to a list of users
     """
     def __init__(self):
-        DTTag.__init__ ( self, 'sendmail', isempty=1, modules=[MailServices])
+        DTTag.__init__(self,
+                       'sendmail',
+                       isempty=1,
+                       modules=[MailServices])
 
     def genCode(self, indent, codeout, tagreg, tag):
         DTCompilerUtil.tagDebug(indent, codeout, tag)
-        args=DTUtil.tagCall(tag, ['to_addrs', 'subject', 'msg', 
-                                  ( 'from_addr', None )] )
+        args=DTUtil.tagCall(tag,
+                            ['to_addrs',
+                             'subject',
+                             'msg', 
+                             ('from_addr',
+                              None)],
+                            kwcol='kw')
+        kwargs=DTCompilerUtil.pyifyKWArgs(tag, args['kw'])
         args=DTCompilerUtil.pyifyArgs(tag, args)
 
         # Ok, call the mail function
-        codeout.write ( indent, 'if %s:' % args['from_addr'] )
-        codeout.write ( indent+4, '__h.MailServices.sendmail ( %s, %s, %s, '
-                                  'from_addr = %s )' % ( args['to_addrs'], 
-                                  args['subject'], args['msg'], 
-                                  args['from_addr'] ) )
-        codeout.write ( indent, 'else:' )
-        codeout.write ( indent+4, '__h.MailServices.sendmail ( %s, %s, %s )' % 
-                                ( args['to_addrs'], args['subject'],
-                                  args['msg'], ) )
+        codeout.write(indent, 'if %s:' % args['from_addr'])
+        codeout.write(indent+4,
+                      ('__h.MailServices.sendmail(%s, %s, %s, '
+                       'from_addr=%s, **%s)') \
+                      % (args['to_addrs'], 
+                         args['subject'],
+                         args['msg'], 
+                         args['from_addr'],
+                         kwargs))
+        codeout.write(indent, 'else:')
+        codeout.write(indent+4,
+                      '__h.MailServices.sendmail(%s, %s, %s, **%s)' \
+                      % (args['to_addrs'],
+                         args['subject'],
+                         args['msg'],
+                         kwargs))
 
         # All done
