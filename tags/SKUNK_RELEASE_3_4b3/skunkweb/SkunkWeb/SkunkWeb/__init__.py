04eed613fa2196b16662fa55dcd2745d4df7e594
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2004-11-11 03:24:03 +0000

    deleting dud tags

--- tags/SKUNK_RELEASE_3_4b3/skunkweb/SkunkWeb/SkunkWeb/__init__.py
+++
@@ -1,8 +0,0 @@
-#  
-#  Copyright (C) 2001 Andrew T. Csillag <drew_csillag@geocities.com>
-#  
-#      You may distribute under the terms of either the GNU General
-#      Public License or the SkunkWeb License, as specified in the
-#      README file.
-#   
-# $Id: __init__.py,v 1.2 2003/05/01 20:45:55 drew_csillag Exp $
--- tags/SKUNK_RELEASE_3_4b4/skunkweb/docs/tutorial/src/sitelibs/hoptime/__init__.py
+++
@@ -1,7 +0,0 @@
-# Time-stamp: <02/10/02 18:39:07 smulloni>
-# $Id: __init__.py,v 1.1 2002/10/06 04:03:44 smulloni Exp $
-
-"""
-package for modules supporting the hoptime application,
-including the hoptime service itself (hoptime.service).
-"""
