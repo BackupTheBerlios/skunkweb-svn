5f6dcc3e038980da441daedbd052ba50a2f1c01b
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2005-02-09 18:20:59 +0000

    added conversion thingie.

--- sandbox/smulloni/pydodev/src/PyDO/operators.py
+++ sandbox/smulloni/pydodev/src/PyDO/operators.py
@@ -11,12 +11,12 @@ string.  The first element of the tuple is the SQL operator, and the
 remaining elements are arguments, which may be atoms or nested tuples,
 which are recursively converted to SQLOperator tuples.
 
-The FIELD class is a helper class which distinguishes field names (and
-names of constants, like NULL) from plain strings, which will appear
-in the SQL output as string literals.  NULL in particular is available
-as a constant, due to the understandable popularity of nullity; it is
-equal to FIELD('NULL').  Another helper class, SET, is available to
-help use the IN operator:
+FIELD is a synonym for CONSTANT, a helper class which distinguishes
+field names (and names of constants, like NULL) from plain strings,
+which will appear in the SQL output as string literals.  NULL in
+particular is available as a constant, due to the understandable
+popularity of nullity; it is equal to CONSTANT('NULL').  Another
+helper class, SET, is available to help use the IN operator:
 
   >>> print SQLOperator(('IN', FIELD('x'), SET(1, 2, 3, 4)))
   (x IN (1, 2, 3, 4))
@@ -35,25 +35,32 @@ to the explicit tuple notation and are exactly equivalent:
   ...           IN(FIELD('e'), SET('Porthos', 'Athos', 'Aramis')))
   (((x = y) OR (a <= (b * (c ^ 2)))) AND (e IN ('Porthos', 'Athos', 'Aramis')))
 
+SQLOperators can also take a conversion function that format data
+values: SQLOperator(('EQ', FIELD('datecolumn'), myDate),
+converter=myFunc).  Nested operators will inherit the conversion
+function from the enclosing operator class if they don't define one
+themselves.
+
 """
 
 
-__all__=['FIELD', 'SET', 'SQLOperator', 'NULL']
+__all__=['FIELD', 'CONSTANT', 'NULL', 'SET', 'SQLOperator' ]
 
-class FIELD(object):
-    """a way to represent a fieldname in a sql expression"""
+class CONSTANT(object):
+    """a way to represent a constant or field name in a sql expression"""
 
-    __slots__=('fieldname',)
+    __slots__=('name',)
     
-    def __init__(self, fieldname):
-        if not isinstance(fieldname, str):
-            raise TypeError, "fieldname must be a string"
-        self.fieldname=fieldname
+    def __init__(self, name):
+        if not isinstance(name, str):
+            raise TypeError, "name must be a string"
+        self.name=name
 
     def __repr__(self):
-        return self.fieldname
+        return self.name
 
-NULL=FIELD('NULL')    
+NULL=CONSTANT('NULL')    
+FIELD=CONSTANT
 
 class SET(object):
     """a way of passing a set into PyDO where clauses (for IN), e.g.:
@@ -76,7 +83,7 @@ class SET(object):
 
 
 class SQLOperator(tuple):
-    def __new__(cls, t):
+    def __new__(cls, t, converter=None):
         L=len(t)
         if not (2 <= L):
             raise ValueError, "invalid SQL condition"
@@ -85,43 +92,66 @@ class SQLOperator(tuple):
             for x in t[1:]:
                 if isinstance(x, tuple):
                     if isinstance(x, SQLOperator):
-                        tl.append(x)
+                        if converter and not x.converter:
+                            x.converter=converter
+                        tl.append(x)                            
                     else:
-                        tl.append(SQLOperator(x))
+                        tl.append(SQLOperator(x, converter))
                 else:
                     tl.append(x)
             t=tuple(tl)
         return tuple.__new__(cls, t)
 
+    def __init__(self, t, converter=None):
+        super(SQLOperator, self).__init__(t)
+        self.converter=converter
+
+    def _convert(self, val):
+        if self.converter:
+            return self.converter(val)
+        return val
     
     def __repr__(self):
-        L=len(self)
-        if L==2:
-            return "(%s %r)" % self
         op=" %s " % self[0]
-        args=map(repr, self[1:])
+        if len(self)==2:
+            return "(%s %r)" % (op, self._convert(self[1]))
+        args=[repr(self._convert(a)) for a in self[1:]]
         return "(%s)" % op.join(args)
 
 
 class MonadicOperator(SQLOperator):
-    def __new__(cls, val):
-        return SQLOperator.__new__(cls, (cls.operator, val))
-    def __init__(self, val):
-        super(MonadicOperator, self).__init__(self, (self.__class__.operator, val))
+    def __new__(cls, val, converter=None):
+        return SQLOperator.__new__(cls, (cls.operator, val), converter)
+    def __init__(self, val, converter=None):
+        super(MonadicOperator, self).__init__((self.__class__.operator, val), converter)
         
 class DyadicOperator(SQLOperator):
-    def __new__(cls, lval, rval):
-        return SQLOperator.__new__(cls, (cls.operator, lval, rval))
+    def __new__(cls, lval, rval, converter=None):
+        return SQLOperator.__new__(cls, (cls.operator, lval, rval), converter)
             
-    def __init__(self, lval, rval):
-        super(DyadicOperator, self).__init__(self, (self.__class__.operator, lval, rval))
+    def __init__(self, lval, rval, converter=None):
+        super(DyadicOperator, self).__init__((self.__class__.operator, lval, rval), converter)
 
 class PolyadicOperator(SQLOperator):
-    def __new__(cls, *values):
-        return SQLOperator.__new__(cls, (cls.operator,)+values)
+    def __new__(cls, *values, **kw):
+        if kw:
+            converter=kw.get('converter')
+            if not converter:
+                raise ValueError, "converter is only keyword argument allowed"
+        else:
+            converter=None
+        return SQLOperator.__new__(cls, (cls.operator,)+values, converter)
     
-    def __init__(self, *values):
-        super(PolyadicOperator, self).__init__(self, (self.__class__.operator,)+values)
+    def __init__(self, *values, **kw):
+        if not values:
+            raise ValueError, "some values required"
+        if kw:
+            converter=kw.get('converter')
+            if not converter:
+                raise ValueError, "converter is only keyword argument allowed"
+        else:
+            converter=None
+        super(PolyadicOperator, self).__init__((self.__class__.operator,)+values, converter)
 
 
 def __makeOperators():
@@ -174,7 +204,6 @@ def __makeOperators():
 __makeOperators()
 del __makeOperators
 
-        
 
 if __name__=='__main__':
     import doctest
