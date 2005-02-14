7bb64141766e478348e8f7940525b9ae46c22b72
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2005-02-14 22:27:52 +0000

    postgresql now works.  It was necessary to override execute() in the psycopg driver, unfortunately.

--- sandbox/smulloni/pydodev/src/PyDO/drivers/psycopgconn.py
+++ sandbox/smulloni/pydodev/src/PyDO/drivers/psycopgconn.py
@@ -1,20 +1,44 @@
 from PyDO.dbi import DBIBase
 from PyDO.exceptions import PyDOError
-
+from PyDO.log import debug
 from psycopg import connect
 
 class PsycopgDBI(DBIBase):
     
     def _connect(self):
-        self.conn=psycopg.connect(self.connectArgs)
+        return connect(self.connectArgs)
+
+
+    def execute(self, sql, values=(), fields=(), qualified=False):
+        """Executes the statement with the values and does conversion
+        of the return result as necessary.
+        result is list of dictionaries, or number of rows affected"""
+        if self.verbose:
+            debug("SQL: %s", sql)
+            debug("bind variables: %s", values)
+        c=self.conn.cursor()
+        if values:
+            c.execute(sql, values)
+        else:
+            c.execute(sql)
+        if c.statusmessage=='SELECT':
+            resultset=c.fetchall()
+        else:
+            resultset=None
+        if not resultset:
+            if c.statusmessage.startswith('INSERT') or c.statusmessage.startswith('UPDATE'):
+                # rowcount doesn't work
+                return int(c.statusmessage.split()[-1])
+            return -1
+        res=self._convertResultSet(c.description, resultset, qualified)
+        c.close()
+        return res    
 
     def getSequence(self, name):
         cur=self.conn.cursor()
-        sql="select nextval(%s)"
-        values=(name,)
+        sql="select nextval('%s')" % name
         if self.verbose:
             debug("SQL: %s", (sql,))
-            debug("bind variables: %s", name)
         cur.execute(sql)
         res=cur.fetchone()
         if not res:
