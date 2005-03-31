67698c5a6907755a38d4b38c2427623ba29e1c24
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2005-03-31 20:01:46 +0000

    provisional version of scope.  Will be changed drastically subsequent to this checkin.

--- sandbox/smulloni/skunk4/src/skunk/config/scope.py
+++ sandbox/smulloni/skunk4/src/skunk/config/scope.py
@@ -1,15 +1,181 @@
+"""
+The ScopedConfig object manages configuration that changes depending on a context dictionary.
+
+The effective value of the configuration depends on:
+
+  * the default configuration dictionary
+  * scope matchers, which cause configuration to vary according to the environment
+  * the context dictionary
+
+To get the current configuration values, call mash(), which returns a dictionary.
+
+TODO:
+
+   simplify this drastically.  The only state the object should store
+   is the matchers (the configuration). Don't store the context in the
+   object at all; it is just a dictionary.  Don't store the overlay
+   either; mash() will pass in (or have passed in into it) a
+   container.  "mash" is also a dumb name since there isn't anything
+   to mash, anymore.
+
+   I'm checking this in just so I don't lose it; the next version will
+   be quite different.
+
+"""
+
+
 from bisect import insort_right
 from threading import Lock
 import fnmatch
 import os
 import re
 
-from skunk.util.decorators import with_lock
+from skunk.util.decorators import with_lock, _share_metadata, call_super
 
-# helper function for decorator
+# private helper function for decorator
 def _getlock(self, *args, **kwargs):
     return self._lock
 
+def adjustconfig(fn):
+    """a decorator to enforce that mutation operations on data
+    structures are followed by appropriate adjustments to the config
+    object that owns them"""
+    def wrapper(self, *args, **kwargs):
+        ret=fn(self, *args, **kwargs)
+        self._config._adjust()
+        return ret
+    _share_metadata(fn, wrapper)
+    return wrapper
+
+class _contextdict(dict):
+    def __new__(cls, config):
+        return dict.__new__(cls)
+    
+    def __init__(self, config):
+        self._config=config
+        self._lock=config._lock
+
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def __setitem__(self, item, value):
+        pass
+
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def __delitem__(self, item):
+        pass
+
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def clear(self):
+        pass
+        
+
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def update(self, d):
+        pass
+
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def setdefault(self, key, failobj=None):
+        pass
+
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def pop(self, key, *args):
+        pass
+
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def popitem(self):
+        pass
+
+
+class _matcherlist(list):
+    def __new__(cls, config):
+        return list.__new__(cls)
+
+    def __init__(self, config):
+        super(_matcherlist, self).__init__()
+        self._config=config
+        self._lock=config._lock
+
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def __setitem__(self, i, item):
+        ""
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def __delitem__(self, i):
+        ""
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def __setslice__(self, i, j, other):
+        ""
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def __delslice__(self, i, j):
+        ""
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def append(self, item):
+        ""
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def insert(self, i, item):
+        ""
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def pop(self, i=-1):
+        ""
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def remove(self, item):
+        ""
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def reverse(self):
+        ""
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def sort(self, *args, **kwds):
+        ""
+    @with_lock(_getlock)
+    @adjustconfig
+    @call_super
+    def extend(self, other):
+        ""
+
+    def __imul__(self, n):
+        raise NotImplementedError
+
+    def __mul__(self, n):
+        raise NotImplementedError
+    
+    def __iadd__(self, n):
+        self.append(n)
+
+    def __radd__(self, other):
+        raise NotImplementedError
+    
 class ScopedConfig(object):
     """
 
@@ -17,11 +183,12 @@ class ScopedConfig(object):
     """
 
     def __init__(self):
-        self.defaults={}
-        self._scopes={}
-        self._stack=[]
-        self.matchers=[]
         self._lock=Lock()
+        self.defaults={}
+        self.context=_contextdict(self)
+        self.matchers=_matcherlist(self)
+
+        self._overlay={}
 
     @with_lock(_getlock)
     def mergeDefaults(self, **kw):
@@ -31,50 +198,44 @@ class ScopedConfig(object):
 
     @with_lock(_getlock)
     def mash(self):
-        """ """
-        masher=self.defaults.copy()
-        for d in self._stack:
-            masher.update(d)
-        return masher
+        """ returns a dictionary with the current configuration. """
+        mashed=self.defaults.copy()
+        mashed.update(self._overlay)
+        return mashed
 
-    @with_lock(_getlock)
-    def scope(self, d):
-        """ TBD """
-        current=self._scopes
-        current.update(d)
-        scopedict={}
+
+    def _adjust(self):
+        """ private function called by other functions that
+        change the context. Those functions should be synchronized;
+        this is not."""
+        self._overlay.clear()
         for m in self.matchers:
-            self._processMatcher(m, scopedict, d)
-            if scopedict:
-                self.push(scopedict)
+            self._processMatcher(m)
 
-    def _processMatcher(self, matcher, scopedict, valdict):
+    def _processMatcher(self, matcher):
+        valdict=self.context
+        scopedict=self._overlay
         if (valdict.has_key(matcher.param) 
             and matcher.match(valdict)
             and matcher.overlay):
             scopedict.update(matcher.overlay)
         for kid in matcher.children:
-            self._processMatcher(kid, scopedict, valdict)
-            
-    def push(self, d):
-        """ """
-        self._stack.append(d)
-
-    def pop(self):
-        """ """
-        return self._stack.pop()
+            self._processMatcher(kid)
 
     def trim(self):
-        """ """
-        del self._stack[:]
+        """remove all scopes"""
+        self.context.clear()
 
     @with_lock(_getlock)
-    def load_from_file(filename):
+    def Include(filename):
+        """loads configuration from a file."""
         filename=os.path.abspath(filename)
         co=compile(s, filename, 'exec')
         exec codeObj in {}, self.defaults
 
-                
+    def Scope(self, *matchers):
+        """adds scope matchers"""
+        self.matchers.extend(matchers)
 
 class ScopeMatcher(object):
     def __init__(self, param, matchObj, overlay, children=None):
@@ -143,6 +304,15 @@ class ScopeMatcher(object):
             # who cares
             return cmp(str(self), str(other))
 
+class CondMatcher(ScopeMatcher):
+
+    def _match(self, other):
+        return self.matchObj(other)
+
+class InMatcher(ScopeMatcher):
+
+    def _match(self, other):
+        return other in self.matchObj
 
 class StrictMatcher(ScopeMatcher):
 
@@ -170,6 +340,16 @@ class RegexMatcher(ScopeMatcher):
     def _match(self, other):
         return isinstance(other, basestring)\
                and self._compiledRegex.match(other)
+
+
+def _createMatcher(matcherClass, paramName, paramVal, kids, kw):
+    m=matcherClass(paramName, paramVal, kw)
+    if kids:
+        m.addChildren(*kids)
+    return m
+
+        
+    
 
 
 # TODO:
--- sandbox/smulloni/skunk4/src/skunk/util/decorators.py
+++ sandbox/smulloni/skunk4/src/skunk/util/decorators.py
@@ -1,10 +1,17 @@
-from __future__ import nested_scopes
 
 def _share_metadata(fn, dec):
     dec.__name__=fn.__name__
     dec.__doc__=fn.__doc__
     dec.__dict__.update(fn.__dict__)
 
+
+def call_super(fn):
+    """decorator that calls the super of an instance method"""
+    def newfunc(self, *args, **kwargs):
+        smeth=getattr(super(self.__class__, self), fn.__name__)
+        return smeth(*args, **kwargs)
+    _share_metadata(fn, newfunc)
+    return newfunc
 
 
 def with_lock(lock):
