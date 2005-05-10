8879ebd852b806d9b39891f372dfe12381e70a2f
smulloni <smulloni@b0d435bb-e9e7-0310-90fb-840138c28313>
2005-05-10 19:46:45 +0000

    little improvements to setup.py

--- trunk/PyDO/setup.py
+++ trunk/PyDO/setup.py
@@ -3,20 +3,32 @@ import sys
 sys.path.insert(0, 'src')
 from PyDO2 import __version__ as version
 description='a Python ORM supporting many RDBMS backends'
+long_description="""
+PyDO is a ORM (Object-Relational Mapper) database access library for
+Python that facilitates writing a Python database access layer.  PyDO
+attempts to be simple, flexible, extensible, and unconstraining.
+""".strip()
+
+platforms="OS Independent"
+
 keywords=['ORM',
           'database',
           'SkunkWeb']
-   
+classifiers=[x for x in (line.strip() for line in  open('CLASSIFIERS')) \
+             if x and not x.startswith('#')]
 
 setup(author='Drew Csillag',
       author_email='drew_csillag@yahoo.com',
       maintainer='Jacob Smullyan',
       maintainer_email='smulloni@smullyan.org',
       description=description,
+      long_description=long_description,
+      classifiers=classifiers,
       keywords=keywords,
+      platforms=platforms,
       license='GPL/BSD',
       name='PyDO',
-      url='http://skunkweb.org/PyDO.html',
+      url='http://skunkweb.org/pydo2.html',
       version=version,
       packages=['PyDO2', 'PyDO2.drivers'],
       package_dir={'' : 'src'})
